6cebc0ab20381c54ead4aade4de1dd119d8d4a40
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-04-28 20:45:56 +0900

    ヘッダーの作成とログアウト機能の作成

--- saito_fb/app/controllers/sessions_controller.rb
+++ saito_fb/app/controllers/sessions_controller.rb
@@ -1,15 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
-
   def create
     user = User.find_by(email:params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      redirect_to user_path(user.id)
+      session[:user_id] = user.id #パスワードが一致していたら、セッションにユーザーIDを代入
+      redirect_to user_path(user.id)#詳細画面にリダイレクトして表示
     else
-    flash.now[:danger]="ログインに失敗しました"
-    render "new"
+      flash.now[:danger]="ログインに失敗しました"
+      render "new"
     end
+  end
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = "ログアウトしました"
+    redirect_to new_session_path
   end
 end
