1c8f0b051788d92b64a5de2d99a4028a3cd786ea
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-05-19 17:15:23 +0900

    email重複登録時にエラーメッセージを表示

--- saito_fb/app/models/user.rb
+++ saito_fb/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 30}
-  validates :email, presence: true, length: {maximum: 30},
+  validates :email, presence: true, uniqueness: true, length: {maximum: 30},
   format:{ with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: {maximum: 8}
   before_validation { email.downcase!}
